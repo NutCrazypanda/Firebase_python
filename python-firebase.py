2d4cc95cb1600ebf4d418ca721daa297ff577c60
NutCrazypanda <56244402+NutCrazypanda@users.noreply.github.com>
2021-06-05 14:05:20 +0700

    Create python-firebase.py

---
+++ python-firebase.py
@@ -0,0 +1,13 @@
+import pyrebase
+
+firebaseConfig = {
+    "apiKey": "AIzaSyDQ1uwMF0E8xVU0Iu-kENoQwE5Ihtr5TxE",
+    "authDomain": "cpanda-firebase-test.firebaseapp.com",
+    'projectId": "cpanda-firebase-test",
+    'storageBucket': "cpanda-firebase-test.appspot.com",
+    'messagingSenderId": "325967061721",
+    'appId": "1:325967061721:web:d513aaf05f1e99c7ee14c8",
+    "measurementId": "G-FBGX5DH0CB"
+  }
+
+firebase=pyrebase.initialize_app(firebaseConfig)
