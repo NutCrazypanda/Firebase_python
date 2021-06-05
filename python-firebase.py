4f150499830f855bebac2a3e31169fcdb666e8fa
NutCrazypanda <56244402+NutCrazypanda@users.noreply.github.com>
2021-06-05 14:36:30 +0700

    Update python-firebase.py

--- python-firebase.py
+++ python-firebase.py
@@ -2,11 +2,12 @@ import pyrebase
 
 firebaseConfig = {
     "apiKey": "AIzaSyDQ1uwMF0E8xVU0Iu-kENoQwE5Ihtr5TxE",
+    "databaseURL":"https://cpanda-firebase-test-default-rtdb.asia-southeast1.firebasedatabase.app/",
     "authDomain": "cpanda-firebase-test.firebaseapp.com",
-    'projectId": "cpanda-firebase-test",
-    'storageBucket': "cpanda-firebase-test.appspot.com",
-    'messagingSenderId": "325967061721",
-    'appId": "1:325967061721:web:d513aaf05f1e99c7ee14c8",
+    "projectId": "cpanda-firebase-test",
+    "storageBucket": "cpanda-firebase-test.appspot.com",
+    "messagingSenderId": "325967061721",
+    "appId": "1:325967061721:web:d513aaf05f1e99c7ee14c8",
     "measurementId": "G-FBGX5DH0CB"
   }
 
@@ -15,5 +16,5 @@ firebase=pyrebase.initialize_app(firebaseConfig)
 firebase_db=firebase.database()
 
 #Insert Data
-firebase_db={"Name":"Nut-Crazypanda","Game":"Battlefield V", "Weapons":["STG-44", "Lung mine"]}
+data={"Name":"Nut-Crazypanda","Game":"Battlefield V", "Weapons":["STG-44", "Lung mine"]}
 print(firebase_db.push(data)) #unique key is generated
